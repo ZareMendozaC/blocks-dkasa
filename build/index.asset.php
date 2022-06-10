b8b01c7bb93921b99e719b0792d48e72115ef478
Gabriel <gsoto@apros.pe>
2022-06-10 13:10:19 -0500

    Cambios

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0b440f53b882be174a7e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0c9cb74fa2b925fb009f');
