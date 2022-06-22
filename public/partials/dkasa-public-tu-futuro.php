1f4220c27522aa6cd7207101f53b8d8015f5f0d2
Gabriel <gsoto@apros.pe>
2022-06-22 12:44:54 -0500

    cambios

--- public/partials/dkasa-public-tu-futuro.php
+++ public/partials/dkasa-public-tu-futuro.php
@@ -26,36 +26,38 @@ $projects = $attributes['projects'];
 		</div>
 		<div class="col-12 px-0">
 			<div class="contenido-contenedor">
+				<div id="slider-tu-futuro">
 				<?php foreach ($projects as $project) : ?>
-					<div class="tf-card">
-						<a href="#">
-							<div class="td-card-front">
-								<div>
-									<img class="" src="<?= $project["mediaUrl"]; ?>" alt="Dibujo">
-								</div>
-								<div class="estado">
-									<p><?= $project["estado"]; ?></p>
-								</div>
-							</div>
-							<div class="tf-card-back">
-								<div>
-									<p class="nombre"><?= $project["nombre"]; ?></p>
-									<p class="direccion"><?= $project["direccion"]; ?></p>
-									<p class="distrito"><?= $project["distrito"]; ?></p>
-									<p class="detalles"><?= $project["detalles"]; ?></p>
-									<p class="metraje"><?= $project["metraje"]; ?></p>
-									<p class="precio"><?= $project["precio"]; ?></p>
-									<p class="estado"><?= $project["estado"]; ?></p>
-									<img class="logo" src="<?= $project["mediaUrlLogo"]; ?>" alt="Dibujo">
-									<div class="d-flex justify-content-end align-items-center">
-										<p class="texto-enlace"><?= $project["textoEnlace"]; ?></p>
-										<img src="<?= site_url("/").'wp-content/uploads/2022/06/Vector.png' ?>" alt="">
+						<div class="tf-card">
+							<a href="#">
+								<div class="td-card-front">
+									<div>
+										<img class="" src="<?= $project["mediaUrl"]; ?>" alt="Dibujo">
+									</div>
+									<div class="estado">
+										<p><?= $project["estado"]; ?></p>
 									</div>
 								</div>
-							</div>
-						</a>
-					</div>
+								<div class="tf-card-back">
+									<div>
+										<p class="nombre"><?= $project["nombre"]; ?></p>
+										<p class="direccion"><?= $project["direccion"]; ?></p>
+										<p class="distrito"><?= $project["distrito"]; ?></p>
+										<p class="detalles"><?= $project["detalles"]; ?></p>
+										<p class="metraje"><?= $project["metraje"]; ?></p>
+										<p class="precio"><?= $project["precio"]; ?></p>
+										<p class="estado"><?= $project["estado"]; ?></p>
+										<img class="logo" src="<?= $project["mediaUrlLogo"]; ?>" alt="Dibujo">
+										<div class="d-flex justify-content-end align-items-center">
+											<p class="texto-enlace"><?= $project["textoEnlace"]; ?></p>
+											<img src="<?= site_url("/").'wp-content/uploads/2022/06/Vector.png' ?>" alt="">
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
 				<?php endforeach; ?>
+				</div>
 			</div>
 		</div>
 		<div class="col-12 px-0">
