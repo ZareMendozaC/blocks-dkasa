b544c901bf819b5a060c6867537fdb630f627167
Gabriel <gsoto@apros.pe>
2022-07-04 17:20:42 -0500

    Cambios

--- public/partials/dkasa-public-testimonios.php
+++ public/partials/dkasa-public-testimonios.php
@@ -17,6 +17,8 @@
  */
 
 $testimonials = $attributes['testimonials'];
+$slider_counter = 0;
+$modal_counter = 0;
 ?>
 <section id="testimonios-front" class="position-relative container-fluid">
     <div class="row">
@@ -26,14 +28,14 @@ $testimonials = $attributes['testimonials'];
         <div class="col-12 px-0 columna-2">
             <div class="contenido-contenedor">
                 <div id="testimonios-slider">
-                    <?php foreach ($testimonials as $testimony) : ?>
+                    <?php foreach ($testimonials as $testimony) : $slider_counter++; ?>
                         <div class="testimonios-slide">
                             <div class="slider-container">
                                 <div class="columna-1" data-aos="fade-zoom-in" data-aos-duration="2000" data-aos-delay="750">
                                     <div class="position-relative">
                                         <img src="<?= $testimony['mediaUrl'] ?>" alt="">
                                         <div class="nombre position-absolute">
-                                            <p ><?= $testimony['nombre'] ?></p>
+                                            <p><?= $testimony['nombre'] ?></p>
                                         </div>
                                     </div>
                                     <p class="proyecto"><?= $testimony['proyecto'] ?></p>
@@ -42,13 +44,16 @@ $testimonials = $attributes['testimonials'];
                                     <p class="descripcion">
                                         <?= $testimony['descripcion'] ?>
                                     </p>
-                                    <a class="texto-enlace" href="">
+                                    <!-- Button trigger modal -->
+                                    <a class="texto-enlace" data-toggle="modal" data-target="#testimonioModal<?= $slider_counter ?>">
                                         <?= $testimony['textoEnlace'] ?>
                                     </a>
+
                                 </div>
                             </div>
-                            <img data-aos="fade-zoom-in" data-aos-duration="2000" data-aos-delay="750" class="comilla-grande" src="<? site_url('/')?>wp-content/uploads/2022/07/Comilla-grande.png" alt="">
-                            <img data-aos="fade-zoom-in" data-aos-duration="2000" data-aos-duration="750" class="comilla-chica" src="<? site_url('/')?>wp-content/uploads/2022/07/Comilla-chica.png" alt="">
+                            <img data-aos="fade-zoom-in" data-aos-duration="2000" data-aos-delay="750" class="comilla-grande" src="<? site_url('/') ?>wp-content/uploads/2022/07/Comilla-grande.png" alt="">
+                            <img data-aos="fade-zoom-in" data-aos-duration="2000" data-aos-duration="750" class="comilla-chica" src="<? site_url('/') ?>wp-content/uploads/2022/07/Comilla-chica.png" alt="">
+                            
                         </div>
                     <?php endforeach; ?>
                 </div>
@@ -58,5 +63,26 @@ $testimonials = $attributes['testimonials'];
             </div>
         </div>
     </div>
+    <?php foreach ($testimonials as $testimony) : $modal_counter++; ?>
 
+    <div class="modal fade" id="testimonioModal<?= $modal_counter ?>" tabindex="-1" aria-labelledby="testimonioModal<?= $modal_counter ?>Label" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="testimonioModal<?= $modal_counter ?>Label">Modal title</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            ...
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php endforeach; ?>
 </section>
